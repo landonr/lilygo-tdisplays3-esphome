e10cda19af54bb3574b2808519f4a6b29ab41c24
Your Name <you@example.com>
2023-06-30 10:34:56 +0200

    Make Reset, CS & DC pin configurable.

--- components/tdisplays3/display.py
+++ components/tdisplays3/display.py
@@ -1,17 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+
+from esphome import pins
 from esphome.components import display
 from esphome.const import (
-  CONF_HEIGHT,
-  CONF_ID,
-  CONF_LAMBDA,
-  CONF_WIDTH,
+    CONF_HEIGHT,
+    CONF_ID,
+    CONF_LAMBDA,
+    CONF_WIDTH,
+    CONF_CS_PIN,
+    CONF_DC_PIN,
+    CONF_RESET_PIN,
+    CONF_NUMBER,
 )
 from . import tdisplays3_ns
 
-DEPENDENCIES = ["esp32"]
-
 AUTO_LOAD = ["psram"]
+DEPENDENCIES = ["esp32"]
 
 CONF_BACKLIGHT = "backlight"
 CONF_LOAD_FONTS = "load_fonts"
@@ -30,9 +35,13 @@ CONFIG_SCHEMA = cv.All(
             cv.Optional(CONF_BACKLIGHT, default=False): cv.boolean,
             cv.Optional(CONF_LOAD_FONTS, default=False): cv.boolean,
             cv.Optional(CONF_LOAD_SMOOTH_FONTS, default=False): cv.boolean,
+            cv.Optional(CONF_RESET_PIN, default=5): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_CS_PIN, default=6): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_DC_PIN, default=7): pins.gpio_output_pin_schema,
         }
     ).extend(cv.polling_component_schema("5s")),
 )
+
 
 async def to_code(config):
     # Add platformio build_flags for the correct TFT_eSPI settings for the T-Display-S3
@@ -46,9 +55,9 @@ async def to_code(config):
     cg.add_build_flag("-DTFT_PARALLEL_8_BIT")
     cg.add_build_flag(f"-DTFT_WIDTH={config[CONF_WIDTH]}")
     cg.add_build_flag(f"-DTFT_HEIGHT={config[CONF_HEIGHT]}")
-    cg.add_build_flag("-DTFT_RST=5")
-    cg.add_build_flag("-DTFT_CS=6")
-    cg.add_build_flag("-DTFT_DC=7")
+    cg.add_build_flag(f"-DTFT_RST={config[CONF_RESET_PIN][CONF_NUMBER]}")
+    cg.add_build_flag(f"-DTFT_CS={config[CONF_CS_PIN][CONF_NUMBER]}")
+    cg.add_build_flag(f"-DTFT_DC={config[CONF_DC_PIN][CONF_NUMBER]}")
     cg.add_build_flag("-DTFT_WR=8")
     cg.add_build_flag("-DTFT_RD=9")
     cg.add_build_flag("-DTFT_D0=39")
@@ -68,12 +77,12 @@ async def to_code(config):
         cg.add_build_flag("-DLOAD_FONT7")
         cg.add_build_flag("-DLOAD_FONT8")
         cg.add_build_flag("-DLOAD_GFXFF")
-    
+
     if config[CONF_LOAD_SMOOTH_FONTS]:
         cg.add_build_flag("-DSMOOTH_FONT")
         cg.add_library("FS", None)
         cg.add_library("SPIFFS", None)
-    
+
     # TODO:
     # PIN_POWER_ON 15
     # Bat_Volt  4
