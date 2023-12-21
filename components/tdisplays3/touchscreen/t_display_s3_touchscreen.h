d0029e5d45c63c6900c02440adef09ba25e24393
Guillem Pagès Gassull <git@guillempages.cat>
2023-12-21 20:31:42 +0100

    Update touchscreen to ESPHome 2023.12.0
    The touchscreen base component had some breaking changes that required adaptations.
    Also the yaml configuration is slightly different, so updated the example-touch.yaml file as well.

--- components/tdisplays3/touchscreen/t_display_s3_touchscreen.h
+++ components/tdisplays3/touchscreen/t_display_s3_touchscreen.h
@@ -3,10 +3,12 @@
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/touchscreen/touchscreen.h"
 #include "esphome/core/component.h"
+#include "esphome/core/version.h"
 
 namespace esphome {
 namespace tdisplays3 {
 
+#if ESPHOME_VERSION_CODE < VERSION_CODE(2023, 12, 0)
 /**
  * Store interrupt related information.
  */
@@ -16,11 +18,18 @@ struct Store {
 
   static void gpio_intr(Store *store);
 };
+#endif  // VERSION_CODE(2023, 12, 0)
 
-class LilygoTDisplayS3Touchscreen : public touchscreen::Touchscreen, public Component, public i2c::I2CDevice {
+class LilygoTDisplayS3Touchscreen : public touchscreen::Touchscreen,
+#if ESPHOME_VERSION_CODE < VERSION_CODE(2023, 12, 0)
+                                    public Component,
+#endif  // VERSION_CODE(2023, 12, 0)
+                                    public i2c::I2CDevice {
  public:
   void setup() override;
+#if ESPHOME_VERSION_CODE < VERSION_CODE(2023, 12, 0)
   void loop() override;
+#endif  // VERSION_CODE(2023, 12, 0)
   void dump_config() override;
 
   void set_interrupt_pin(InternalGPIOPin *pin) { this->interrupt_pin_ = pin; }
@@ -37,7 +46,11 @@ class LilygoTDisplayS3Touchscreen : public touchscreen::Touchscreen, public Comp
   int16_t x_offset_;
   int16_t y_offset_;
   uint16_t firmware_version_;
+#if ESPHOME_VERSION_CODE < VERSION_CODE(2023, 12, 0)
   Store store_;
+#else
+  void update_touches() override;
+#endif  // VERSION_CODE(2023, 12, 0)
 };
 
 }  // namespace tdisplays3
